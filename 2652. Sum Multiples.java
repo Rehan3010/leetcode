27c93cacf0ad24a9d45f2e002e2832a8653427c0
Rehan3010 <101172131+Rehan3010@users.noreply.github.com>
2023-09-30 23:28:59 +0530

    Update 2652. Sum Multiples.java

--- 2652. Sum Multiples.java
+++ 2652. Sum Multiples.java
@@ -11,3 +11,18 @@ class Solution {
         return sum - 1;
     }
 }
+
+
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i = 1; i<=n; i++)
+        {
+            if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
