cb5882501d45c84ad9a5ba154e6e12aa6394d1f7
Rehan3010 <101172131+Rehan3010@users.noreply.github.com>
2023-10-28 23:11:21 +0530

    Create 2544. Alternating Digit Sum.java

---
+++ 2544. Alternating Digit Sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int ans = 0;
+        int sign = 1;
+        while (n > 0) {
+            sign *= -1;
+            ans += sign * n % 10;
+            n /= 10;
+        }
+        return sign * ans;
+    }
+}
