af8f23f713cff439a59037369ec7c22e0d534506
Rehan3010 <101172131+Rehan3010@users.noreply.github.com>
2024-08-23 17:10:41 +0530

    Create 204. Count Primes.java

---
+++ 204. Count Primes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPrimes(int n) {
+       if(n <= 2) {
+        return 0;
+       }
+    boolean[] isprime = new boolean[n];
+    Arrays.fill(isprime,true);
+    isprime[0] = isprime[1] = false;
+    for(int i = 2; i * i < n; i++){
+        if(isprime[i]) {
+            for(int j = i*i; j < n; j += i) {
+                isprime[j] = false;
+            }
+        }
+    }
+    int count = 0;
+    for(int i = 2; i < n; i++) {
+        if(isprime[i]) {
+            count++;
+        }
+    }
+    return count;
+    }
+}
